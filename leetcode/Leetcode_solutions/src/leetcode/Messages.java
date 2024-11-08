7283f932f8b40c0ea286445d45c5215019032b6a
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2024-11-08 14:25:46 +0200

    message

--- leetcode/Leetcode_solutions/src/leetcode/Messages.java
+++ leetcode/Leetcode_solutions/src/leetcode/Messages.java
@@ -5,32 +5,47 @@ import java.util.Map;
 
 public class Messages {
     public static String largestWordCount(String[] messages, String[] senders) {
-        HashMap<String, Integer> table = new HashMap<String, Integer>();
+        HashMap<String, Integer> table = new HashMap<String, Integer>();///String is the sender and Integer is their word count
         for(int i = 0; i < senders.length; i++){
-            int helper = 0;
-            if(table.containsKey(senders[i])){
+            int helper = 0; ///So that we can add the users other wordcount
+            if(table.containsKey(senders[i])){///Checking if the sender is mendtioned a second time
                 helper = table.get(senders[i]);
 
             }
-            int wc = 0;
-            for(char c : messages[i].toCharArray()){
+            int wc = 0;///wc = word count
+            for(char c : messages[i].toCharArray()){///Checking how many spaces between words
                 if(c ==' '){
                     wc++;
                 }
             }
-            table.put(senders[i],wc + helper +1); ///
+            table.put(senders[i],wc + helper +1); ///we add +1 since word count is 1 larger than the spaces between
 
         }
-        ///Finding largest the sender with the larges wordcount
+        ///Finding the sender with the largest wordcount/lexicographically largest name
         int max = 0;
         String Sender = "";
         for(Map.Entry<String, Integer> entry : table.entrySet()){
             if(entry.getValue() > max){
-                Sender = entry.getKey();
                 max = entry.getValue();
             }
         }
+        for(Map.Entry<String, Integer> entry : table.entrySet()){
+            if(entry.getValue() == max){
+                Sender = CompareNames(Sender,entry.getKey());
+            }
+        }
+
 
         return Sender;
     }
+    ///Lexicographical comparison of two names
+    public static String CompareNames(String name1, String name2) {
+
+        if(name1.compareTo(name2) > 0){
+            return name1;
+        }
+        return name2;
+
+    }
+
 }
