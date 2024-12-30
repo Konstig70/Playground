d1ce174437bea921768efecd5a3c44e62ce61949
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2024-12-30 19:04:59 +0200

    message

--- cs2_stat_Tracker/cs2_statTracker/TrackerApp.cs
+++ cs2_stat_Tracker/cs2_statTracker/TrackerApp.cs
@@ -22,15 +22,17 @@ public class TrackerApp
     /// </summary>
     public static async Task Main()
     {
+        string directory = AppDomain.CurrentDomain.BaseDirectory;
+        string filepath = Path.Combine(directory, "stats.xml");
+        Console.WriteLine("XML document saved to: " + filepath);
         Console.Write("Please enter your steamId: ");
         string steamId = Console.ReadLine()?.Trim();
         Console.Clear();
         Console.Write("Please enter your steam web API key: ");
         string steamApiKey = Console.ReadLine()?.Trim();
+        
         Console.Clear();
-        string directory = AppDomain.CurrentDomain.BaseDirectory;
-        string filepath = Path.Combine(directory, "stats.xml");
-        Console.WriteLine("XML document saved to: " + filepath);
+        
         APICaller apiCaller = new APICaller(steamApiKey, steamId);
         await apiCaller.Call(new XDocument());
         //Tilastot, joita halutaan seurata
