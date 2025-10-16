01c7998a5e95a519623b3268f86e802e799d964f
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-10-16 17:22:33 +0300

    added a start for checking the http request

--- httpPalvelin/src/httpkasittelija.rs
+++ httpPalvelin/src/httpkasittelija.rs
@@ -1,9 +1,12 @@
 //Käsittelee http pyynnön ja palauttaa sitä vastaavan viestin, eli virheen tai html tiedoston.
 pub fn kasittele_pyynto(viesti: &str) -> &str {
-    let mut i = 1;
-    for osa in viesti.split("\n") {
-        println!("{i}. osa: {osa}");
-        i += i +1;
+    let osat: Vec<&str> = viesti.split("\n").collect();
+    let pyynto: Vec<&str> = osat[0].split(" ").collect();
+    if pyynto.len() < 3 {
+        return "400 Bad Request";
+    }
+    match pyynto[0] {
+
     }
     ""
 } 
