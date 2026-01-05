5290ef5e90bfadfd2841350b8902532ffbe95bd4
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2026-01-05 14:53:32 +0200

    modified synthethicvideo detector page

--- httpPalvelin/src/httpkasittelija.rs
+++ httpPalvelin/src/httpkasittelija.rs
@@ -66,6 +66,7 @@ pub fn kasittele_pyynto(viesti: &str) -> Vec<u8> {
     //Voitaisiin suoraan unwrap, sillä ei "pitäisi" olla mahdollista, että resursissa ei ole tässä
     //vaiheessa .-merkkiä, mutta otetaan cloudflaresta opiksi ja käytetään kuitenkin unwrap_or_else
     let jaettu_polku = resurssi.split_at_checked(resurssi.rfind(".").unwrap_or_else(|| resurssi.len()) + 1);
+    //Nyt jos ei löydetty .-merkkiä niin 
     let loppu = match jaettu_polku {
         Some(arvo) => arvo.1,
         None => return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 48\r\nConnection: close\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>".to_string().into_bytes(),
